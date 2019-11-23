8f32e9b9c6d18a0f6cf336486078e7c4592a03a3
marwan harb <marwanharb65@outlook.com>
2019-11-23 18:21:18 +0200

    Circle class added and simpleSubscriber classes removed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
-
+/*Implementors of circleCir and CircleVol should 
+ * add their methods to cirlce class and class it
+ * in notify subscriber class,the rest should implement 
+ * their methods in their special classes
+ */
 public class Main {
+	//add your object of class in the array below
 	private static ISubscriber subscribers [] = {
-			new SimpleSubscriber(),
-			new ReallySimpleSubscriber(),
+			new Circle()
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
@@ -11,7 +15,7 @@ public class Main {
 			mathTopic.addSubscriber(sub);
 		}
 		Scanner sc = new Scanner(System.in);
-		String input = sc.next();
+		double input = sc.nextDouble();
 		mathTopic.dispatchEvent(input);
 	}
 }

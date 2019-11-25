90a430ec4853786a58aba6bfc3287e1b34973be0
mohamedHessuin <eng.mohamedhessuin@Gmail.com>
2019-11-25 15:43:07 +0200

    updated my class and main

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,8 @@ public class Main {
 			new Circle(),//needs editing before program could start
 			new twoPowerN(),
 			new CircleCircummatance(),
-      new SummationSeries()
+      new SummationSeries(),
+      new multiplicationSeries()
 
 	};
 	public static void main(String[] args) {

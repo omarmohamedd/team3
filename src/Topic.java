b4dcd605720c4d77d315e0bada1b204d6eb47412
OSAKAMAHA <osamaakhader@gmail.com>
2019-11-25 08:23:11 +0200

    i changed the observer method to pull instead of pull and added my class

--- src/Topic.java
+++ src/Topic.java
@@ -2,24 +2,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Topic {
-	
+
 	private List<ISubscriber> subscribers;
-	
+	private double value;
 	public Topic() {
 		subscribers = new ArrayList<ISubscriber>();
 	}
-	
 	public void addSubscriber(ISubscriber sub) {
 		subscribers.add(sub);
 	}
-	
+	public void setValue(double value){
+        this.value = value;
+    }
+    public double getValue(){
+        return value;
+    }
 	public List<ISubscriber> getSubscribers() {
 		return subscribers;
 	}
-	
-	public void dispatchEvent(double input) {
+	public void dispatchEvent() {
 		for (ISubscriber sub : this.getSubscribers()) {
-			sub.notifySubscriber(input);
+			sub.notifySubscriber(this);
 		}
 	}
 }

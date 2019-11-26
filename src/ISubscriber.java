8d79e4854745030bdf9172f46cccf2d40b588872
omarmohamedd <42842970+omarmohamedd@users.noreply.github.com>
2019-11-26 11:30:37 -0800

    Update ISubscriber.java

--- src/ISubscriber.java
+++ src/ISubscriber.java
@@ -1,28 +1,4 @@
 
-public abstract class lucasSeries extends Thread implements ISubscriber{
-    
- private int n;
-    public int cas() 
-    { 
-
-        if (n == 0) 
-            return 2; 
-        if (n == 1) 
-            return 1; 
-
-
-        return lucas(n - 1)  + lucas(n - 2); 
-        system.out.println("lucas "+ lucas(input));
-        
-    }
-
-public void run(){ 
-    lucas();
-}
-    public void notifySubscriber(Topic topic){ 
-        input=(int)topic.getvalue();
-        this.start();
-    }
-
-
+public interface ISubscriber {
+	public abstract void notifySubscriber(Topic topic);
 }

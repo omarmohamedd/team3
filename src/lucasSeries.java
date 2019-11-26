b31ecc04c2dffad19a1f229a3fdf0268715163fc
omarmohamedd <42842970+omarmohamedd@users.noreply.github.com>
2019-11-26 11:26:58 -0800

    Update lucasSeries.java

--- src/lucasSeries.java
+++ src/lucasSeries.java
@@ -1,4 +1,28 @@
 
-public class lucasSeries {
+public abstract class lucasSeries extends Thread implements ISubscriber{
+    
+ private int n;
+    public int lucas(int n) 
+    { 
+
+        if (n == 0) 
+            return 2; 
+        if (n == 1) 
+            return 1; 
+
+
+        return lucas(n - 1)  + lucas(n - 2); 
+       
+        
+    }
+
+public void run(){ 
+     system.out.println("lucas "+ lucas(n));
+}
+    public void notifySubscriber(Topic topic){ 
+        input=(int)topic.getvalue();
+        this.start();
+    }
+
 
 }
